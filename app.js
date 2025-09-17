633cf777f1da946f3f707f041bcffacd5cc93f7e
sakshiS6 <sakshimishra2004545@gmail.com>
2025-09-17 19:47:25 +0530

    updates

--- app.js
+++ app.js
@@ -1,30 +1,27 @@
 const express = require("express");
-const socket = require("socket.io");
 const path = require("path");
+const socket = require("socket.io");
 
 const app = express();
 
-// Serve public folder
+// Serve static files
 app.use(express.static(path.join(__dirname, "public")));
 
 // Serve index.html at root
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
-
-const server = app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const server = app.listen(PORT, () => console.log("Server running on port " + PORT));
 
 // Socket.io setup
 const io = socket(server);
 
 io.on("connection", (socket) => {
-  console.log("Socket connected");
+    console.log("Socket connected");
 
-  socket.on("beginPath", (data) => io.sockets.emit("beginPath", data));
-  socket.on("drawStroke", (data) => io.sockets.emit("drawStroke", data));
-  socket.on("redoUndo", (data) => io.sockets.emit("redoUndo", data));
+    socket.on("beginPath", (data) => io.sockets.emit("beginPath", data));
+    socket.on("drawStroke", (data) => io.sockets.emit("drawStroke", data));
+    socket.on("redoUndo", (data) => io.sockets.emit("redoUndo", data));
 });
