f8510c040297d2cdc3408bbfd1625c75689c8e36
sakshiS6 <sakshimishra2004545@gmail.com>
2025-09-17 19:07:41 +0530

    whiteboard

--- backend/app.js
+++ backend/app.js
@@ -1,29 +1,35 @@
-const express = require("express"); // Access
+const express = require("express");
 const socket = require("socket.io");
+const path = require("path");
 
-const app = express(); //Initialized and server ready
+const app = express();
 
 app.use(express.static("public"));
 
-let port = process.env.PORT || 5000;
-let server = app.listen(port, () => {
-    console.log("Listening to port" + port);
-})
+const port = process.env.PORT || 5000;
 
-let io = socket(server);
+const server = app.listen(port, () => {
+    console.log("Listening on port " + port);
+});
+
+const io = socket(server);
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 io.on("connection", (socket) => {
     console.log("Made socket connection");
-    // Received data
+
     socket.on("beginPath", (data) => {
-        // data -> data from frontend
-        // Now transfer data to all connected computers
         io.sockets.emit("beginPath", data);
-    })
+    });
+
     socket.on("drawStroke", (data) => {
         io.sockets.emit("drawStroke", data);
-    })
+    });
+
     socket.on("redoUndo", (data) => {
         io.sockets.emit("redoUndo", data);
-    })
-})
+    });
+});
